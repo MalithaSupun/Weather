eb090ab3768ec70d4aae1e5201ddd0101ba53aad
Malitha Supun <malithasupun@example.com>
2024-07-03 23:59:04 +0530

    updated

--- Weather/Mannagers/LocationMannager.swift
+++ Weather/Mannagers/LocationMannager.swift
@@ -7,3 +7,24 @@
 
 import Foundation
 import CoreLocation
+
+class LocationMannager: NSObject,ObservableObject,CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        <#code#>
+    }
+}
