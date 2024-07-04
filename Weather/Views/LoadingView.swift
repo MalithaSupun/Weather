28cdac2433725f17f006f7ed337da6dfd431dec7
Malitha Supun <malithasupun2@gmail.com>
2024-07-05 01:57:50 +0530

    updated

---
+++ Weather/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Weather
+//
+//  Created by Malitha Supun on 2024-07-05.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
