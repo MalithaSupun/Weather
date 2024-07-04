2274a14671fd70e3e9c3f7cb5229cee292bc17b2
Malitha Supun <malithasupun@example.com>
2024-07-04 22:47:16 +0530

    add new views group

---
+++ Weather/Views/WelocomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelocomeView.swift
+//  Weather
+//
+//  Created by Malitha Supun on 2024-07-04.
+//
+
+import SwiftUI
+
+struct WelocomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelocomeView()
+}
