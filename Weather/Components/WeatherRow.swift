46706c3abef0f3826cd43489fb7c5acf62a5f469
Malitha Supun <malithasupun2@gmail.com>
2024-07-06 00:27:10 +0530

    85% Done

---
+++ Weather/Components/WeatherRow.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherRow.swift
+//  Weather
+//
+//  Created by Malitha Supun on 2024-07-06.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherRow()
+}
